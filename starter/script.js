bb77940ebd898f340ce306a1164d9c5c0f414df7
ayindesadiq01 <ayindesadiq01@gmail.com>
2023-08-06 14:34:28 +0100

    map

--- starter/script.js
+++ starter/script.js
@@ -13,3 +13,37 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 // CDNs - Content Delivery Network
 
+// USING THE GEOLOCATION API
+
+if(navigator.geolocation) {
+ navigator.geolocation.getCurrentPosition(function(position){
+ console.log(position);
+
+ //WORKING TO GET A PRESENT POSITION
+ const {latitude} =position.coords
+ // const latitude = position.coords.latitude
+ const {longitude} = position.coords; 
+ console.log(latitude, longitude)
+ // console.log(`https://www.google.com/maps/@${9.060352},${7.4678272},13z?entry=ttu`)
+
+ // how to display a map using a third party libariry called LeafLet
+
+ const coords = [latitude, longitude]
+
+ const map = L.map('map').setView(coords, 13);
+
+ //TILELAYER
+ L.tileLayer(
+  'https://tile.openstreetmap.org/{z}/{x}/{y}.png', 
+  {
+   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  // MARKER
+  const marker = L.marker(coords).addTo(map);
+  marker.bindPopup('A Pretty').openPopup()
+}, function(){
+ alert("Can't have access to location")
+})
+}
+
